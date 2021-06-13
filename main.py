79db240b525464da9994599944c9caec515a6d28
nazar1ous <kuspys@ucu.edu.ua>
2021-06-13 20:44:13 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from agent_structures import CarEnv
+from predictors import make_prediction
+import time
+import cv2
+
+# Initialization
+carEnv = CarEnv()
+carEnv.reset()
+for i in range(100):
+    data = carEnv.front_camera
+    result = make_prediction(data)
+    carEnv.make_step(*result)
+    # carEnv.make_step(5, 0)
+    cv2.imwrite(f'./data/{i}.png', data)
+    time.sleep(0.1)
+    # pass
+    # data = carEnv.get_data()
+
+    # print(i)
---
+++ predictors.py
@@ -0,0 +1,3 @@
+def make_prediction(data):
+    # throttle, steer
+    return 5, 0
